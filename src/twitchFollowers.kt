103be82df1bac21a066f6e3d03cde61cdd960411
mart <martddd@gmail.com>
2016-10-10 01:31:34 +0300

    Добавление jsoup библиотеки

--- src/twitchFollowers.kt
+++ src/twitchFollowers.kt
@@ -1,3 +1,16 @@
+
+import org.jsoup.Jsoup
+
 fun main(args: Array<String>){
-    println("Эта программа будет выводить сообщение о новых followers")
+//    println("Эта программа будет выводить сообщение о новых followers")
+
+    var base_url = "http://www.foreca.ru/Russia/Smolensk"
+
+
+    val doc = Jsoup.connect(base_url).get()
+
+    val newsHeadlines = doc.select("right txt-tight")
+
+    print(newsHeadlines)
+
 }
